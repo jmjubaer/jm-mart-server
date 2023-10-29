e2e814070813ac36b617ac94b16ef3aab8e13d80
jmjubaer <jmjubaer3927@gmail.com>
2023-10-29 19:56:50 +0600

    add JWT

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 // const stripe = require("stripe")(process.env.SECRET_KEY_STRIPE);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+      return res.status(401).send({ message: "Unauthorize access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.VITE_TOKEN, (err, decoded) => {
+      if (err) {
+          return res.status(403).send({ message: "Forbidden access" });
+      } else {
+          req.decoded = decoded;
+          next();
+      }
+  });
+};
 
 app.get("/", async (req, res) => {
     res.send("Jm Mart server Running ........");
@@ -41,6 +57,15 @@ async function run() {
     const productsCollection = client.db("jm-mert").collection('products')
     
 
+//Verify related API
+        app.post("/jwt", async (req, res) => {
+          const user = req.body;
+          const token = jwt.sign(user, process.env.VITE_TOKEN, {
+              expiresIn: "1h",
+          });
+          res.send({ token });
+      });
+
 // User Api
     app.post("/user", async (req, res) => {
       const user = req.body;
@@ -59,7 +84,7 @@ async function run() {
         const result = await productsCollection.find().toArray();
         res.send(result);
       })
-  
+      
   
   // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
